aea3fceccd3626d19b9b07e7d636eef83648a917
Garrett Vangilder <garrett.vangilder@gmail.com>
2019-08-23 10:35:52 -0400

    fixes script

--- .circleci/setup-env.sh
+++ .circleci/setup-env.sh
@@ -5,8 +5,8 @@ case $CIRCLE_BRANCH in
         export CLOUDFRONT_DISTRIBUTION_ID="E2QBWI5C3M8RR2"
         ;;
     "master")
-        export AWS_BUCKET_NAME="dev.vuewatch.com"
         export BUILD_NAME="prod"
+        export AWS_BUCKET_NAME="vuewatch.com"
         export CLOUDFRONT_DISTRIBUTION_ID="E1RQG2TMWYYQNZ"
         ;;
 esac
