089d9712bed3eba0fd18d80c045ecfa6d8d1d541
Garrett Vangilder <garrett.vangilder@gmail.com>
2019-08-23 10:30:45 -0400

    adds config

--- .circleci/setup-env.sh
+++ .circleci/setup-env.sh
@@ -1,8 +1,12 @@
 case $CIRCLE_BRANCH in
     "development")
         export BUILD_NAME="dev"
+        export AWS_BUCKET_NAME="dev.vuewatch.com"
+        export CLOUDFRONT_DISTRIBUTION_ID="E2QBWI5C3M8RR2"
         ;;
     "master")
+        export AWS_BUCKET_NAME="dev.vuewatch.com"
         export BUILD_NAME="prod"
+        export CLOUDFRONT_DISTRIBUTION_ID="E1RQG2TMWYYQNZ"
         ;;
 esac
