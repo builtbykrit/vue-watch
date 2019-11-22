f2432597c5504debf7759f725da82f5219875993
Austin Carr <austin.carr@builtbykrit.com>
2019-11-22 13:16:11 -0500

    Adds Header component.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="app">
+    <Header />
     <router-view/>
   </div>
 </template>
+<script>
+import Header from '@/components/Header.vue';
 
+export default {
+  name: 'App',
+  components: {
+    Header,
+  },
+};
+</script>
 <style lang="scss">
 </style>
