866497b7716eb0f3554ba0cb161e26d0ecd9df38
Garrett Vangilder <garrett.vangilder@gmail.com>
2019-11-22 11:15:02 -0500

    adds tags

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="list w-2/3 m-auto my-32">
-    <div class="mb-2">
-      <Count
-        :count='pluginCount'
-        v-if="pluginsSortedByScore.length > 0"
-      />
+  <div class="list grid-container one-four-one my-16 px-4">
+    <div class="tag">
+      <p class="mb-6 mx-1 text-gray-500 font-medium text-lg">Tags</p>
+      <TagList :tags="['State Management', 'UI Utility', 'Time & Date']" />
     </div>
     <div v-if="pluginsSortedByScore.length > 0">
+      <div class="mb-2">
+        <Count
+          :count='pluginCount'
+          v-if="pluginsSortedByScore.length > 0"
+        />
+      </div>
       <Peep :key="plugin.id" :plugin='plugin' v-for="plugin in pluginsSortedByScore"/>
     </div>
   </div>
@@ -16,12 +20,14 @@
 import { mapActions, mapGetters } from 'vuex';
 import Count from '@/components/Plugins/Count.vue';
 import Peep from '@/components/Plugins/Peep.vue';
+import TagList from '@/components/Plugins/TagList.vue';
 
 export default {
   name: 'List',
   components: {
     Count,
     Peep,
+    TagList,
   },
   computed: {
     ...mapGetters([
